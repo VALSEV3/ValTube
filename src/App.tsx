233473bc8106b7657a29164fbc77f0a8a98c3b2b
VALSEV3 <valeri.sevyan@tumo.org>
2024-11-02 17:55:01 +0400

    base href

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/home';
 import SignUp from './components/signUp/login';
 import Profile from './components/profile/profile';
-function App() {
- 
 
+function App() {
   return (
-  <>
-<Router>
-  <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/signup" element={<SignUp />}/>
-    <Route path="/profile" element={<Profile/>}/>
-  </Routes>
-</Router>
-  </>
-  )
+    <Router basename="/ValTube"> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
